47ed9643c29ae5f8a91b402e9917fea35117b271
Hamdan <hamdankhan212@gmail.com>
2022-12-26 09:12:13 +0500

    Before context api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
+import About from "./components/About";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <h1>iNotebook</h1>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route exact path="/about" element={<About />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
